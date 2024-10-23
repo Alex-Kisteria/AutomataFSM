a2ade202675713cfaca942e8a687996aa53757bd
Alexsus <124175680+Alex-Kisteria@users.noreply.github.com>
2024-10-23 20:27:39 +0800

    small update

--- KISTERIA_fsm.cpp
+++ KISTERIA_fsm.cpp
@@ -5,9 +5,9 @@ int main() {
     int state = 0; 
     char input;
 
-    // FSM table
+    // Transition table
     int table[6][4] = {
-        {1, 4, 4, 4},  // state 0 (start with motor)
+        {1, 4, 4, 4},  // state 0 
         {5, 5, 2, 3},  // state 1
         {5, 5, 1, 3},  // state 2
         {5, 5, 4, 4},  // state 3
@@ -17,7 +17,7 @@ int main() {
 
     cout << "Hello Cheska! Please choose your first mode of transportation.\n";
 
-    while (true) {  // Infinite loop until 'x' is entered
+    while (true) {  
         cout << "Enter your mode of transportation (a = motor, b = jeep, c = ferry, d = van, x = exit): ";
         cin >> input;
         
@@ -28,30 +28,29 @@ int main() {
             break;  // Exit the loop
         }
 
-        int inputColumn = -1;  // to store which column to use
+        int inputColumn = -1;  //used for the column
 
         // Determine input column based on input
         switch (input) {
             case 'a': 
-                inputColumn = 0; break;  // motor
+                inputColumn = 0; break;  // column 0 para sa motor
             case 'b': 
-                inputColumn = 1; break;  // jeep
+                inputColumn = 1; break;  // column 1 para sa jeep
             case 'c': 
-                inputColumn = 2; break;  // ferry
+                inputColumn = 2; break;  // column 2 para sa ferry
             case 'd': 
-                inputColumn = 3; break;  // van
+                inputColumn = 3; break;  // column 3 para sa van
             default:
                 cout << "Invalid input. Try again.\n";
                 continue;
         }
 
-        // Move to the next state based on the current state and input
-
+        // Move sa next state based sa current state and input
         state = table[state][inputColumn];
 
     }
     
-    // Check final and dead states
+    // Check final or dead state
         if (state == 5) {
             cout << "You have successfully reached your destination!\n";
         } else {
